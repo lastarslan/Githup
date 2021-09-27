63330605231615fe36e940f4a356d102320d0eff
MTA <MTA@DESKTOP-T4BOQOS>
2021-09-27 18:58:40 +0300

    Add project files.

---
+++ Githup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Githup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
